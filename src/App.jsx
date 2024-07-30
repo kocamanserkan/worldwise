799bbcefe1e494775bf7f43879a2a56d688f4cfa
Serkan Kocaman <kocaman.serkan98@gmail.com>
2024-07-30 22:23:14 +0300

    adding pages

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,12 @@ function App() {
           <Route path="product" element={<Product />}></Route>
           <Route path="pricing" element={<Pricing />}></Route>
           <Route path="/login" element={<Login />}></Route>
-          <Route path="app" element={<AppLayout />}></Route>
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<p>LIST</p>}></Route>
+            <Route path="cities" element={<p>List of cities</p>}></Route>
+            <Route path="countries" element={<p>Countries</p>}></Route>
+            <Route path="form" element={<p>form</p>}></Route>
+          </Route>
           <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
       </BrowserRouter>
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,11 @@
-import AppNav from "../components/AppNav";
-
+import Map from "../components/Map";
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
 function AppLayout() {
   return (
-    <div>
-      <AppNav></AppNav>
-      <p>App</p>
+    <div className={styles.app}>
+      <Sidebar></Sidebar>
+      <Map></Map>
     </div>
   );
 }
