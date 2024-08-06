f7839bd884102d7b48b57f8c2356c8d1f9c150a8
Serkan Kocaman <kocaman.serkan98@gmail.com>
2024-08-06 23:44:34 +0300

    Cities Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Product from "./pages/Product";
@@ -11,59 +10,35 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-
-const BASE_URL = "http://localhost:9000";
+import { CitiesProvider } from "./contexts/CitiesContext";
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch {
-        alert("There was an error while fetching data");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchCities();
-  }, []);
-
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="product" element={<Product />}></Route>
-          <Route path="pricing" element={<Pricing />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="app" element={<AppLayout />}>
-            <Route
-              index
-              element={<Navigate to="cities" replace></Navigate>}
-            ></Route>
-            <Route
-              path="cities"
-              element={
-                <CityList cities={cities} isLoading={isLoading}></CityList>
-              }
-            ></Route>
-            <Route path="cities/:id" element={<City></City>}></Route>
-            <Route
-              path="countries"
-              element={<CountryList cities={cities}></CountryList>}
-            ></Route>
-            <Route path="form" element={<Form></Form>}></Route>
-          </Route>
-          <Route path="*" element={<PageNotFound />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="product" element={<Product />}></Route>
+            <Route path="pricing" element={<Pricing />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="app" element={<AppLayout />}>
+              <Route
+                index
+                element={<Navigate to="cities" replace></Navigate>}
+              ></Route>
+              <Route path="cities" element={<CityList></CityList>}></Route>
+              <Route path="cities/:id" element={<City></City>}></Route>
+              <Route
+                path="countries"
+                element={<CountryList></CountryList>}
+              ></Route>
+              <Route path="form" element={<Form></Form>}></Route>
+            </Route>
+            <Route path="*" element={<PageNotFound />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
     </div>
   );
 }
---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useState, useEffect } from "react";
+
+const BASE_URL = "http://localhost:9000";
+const CititesConxtext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error while fetching data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
+  return (
+    <CititesConxtext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CititesConxtext.Provider>
+  );
+}
+
+export { CitiesProvider };
